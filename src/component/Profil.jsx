27f77ae79c5bd6e4837c87abb2be8186ab6f7c62
aidouda <82780195+aidouda@users.noreply.github.com>
2021-06-25 18:17:39 +0200

    Add files via upload

---
+++ src/component/Profil.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Profil = () => {
+    return (
+        <div>
+            <h1 style={{color: "red"}}> hello React </h1>
+            <p>ReactJS offers graceful solutions to some of front-end programming’s most persistent issues, allowing you to build dynamic and interactive web apps with ease. It’s fast, scalable, flexible, powerful, and has a robust developer community that’s rapidly growing. There’s never been a better time to learn React. </p>
+          
+        </div>
+    )
+}
+
+export default Profil
+
+
